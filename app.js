e14475aa7a3bb96b6a5a4a906815a3ba664f566b
Patrick Yeo <patrick.yeo@liferay.com>
2018-08-18 15:56:15 -0700

    More components to clay 2 and remove clay 1 stuff

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 var sitePageCtrl = (req, res, next) => {
 	var page = req.params.page;
 	var pageExists = false;
-	var sitePages = ['alert', 'badge', 'blockquote', 'breadcrumb', 'button', 'card', 'dropdown', 'input-custom', 'input-group', 'input-validation', 'input', 'label', 'links', 'list-group', 'loading-animations', 'management-bar', 'menubar', 'modal', 'multi-step-nav', 'nav-pills', 'nav-tabs', 'nav-underline', 'nav', 'navbar', 'pagination', 'panel', 'popover', 'progress-bar', 'sheet', 'sidebar', 'sticker', 'table', 'tbar', 'timeline', 'toggle-switch', 'tooltip', 'typography', ];
+	var sitePages = [ 'globals', 'alert', 'badge', 'breadcrumb', 'button', 'card', 'dropdown', 'input-custom', 'input-group', 'input-validation', 'input', 'label', 'links', 'list-group', 'loading-animations', 'management-bar', 'menubar', 'modal', 'multi-step-nav', 'nav-pills', 'nav-tabs', 'nav-underline', 'nav', 'navbar', 'pagination', 'panel', 'popover', 'progress-bar', 'sheet', 'sidebar', 'sticker', 'table', 'tbar', 'timeline', 'toggle-switch', 'tooltip', 'typography', ];
 
 	for (var i = 0; i < sitePages.length; i++) {
 		if (sitePages[i] === page) {
